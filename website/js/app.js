4649499183ebb77f79e82135148959e77963ab07
Alex <alex.lillo@gmail.com>
2021-06-30 20:56:00 +0100

    checks if US zip code, shows weather on screen

--- website/js/app.js
+++ website/js/app.js
@@ -1,24 +1,51 @@
 
+const updateUIWithWeather = (data) => {
+  console.log(data);
+  const target = document.querySelector('.help-tip');
+  if (data) {
+    target.innerText = `${data.name}, ${data.weather[0].description} ${data.main.temp}C `;
+  }
+  else {
+    target.innerText = "Can't find that zip code";
+  }
+}
+
+
 // check there's some postcode first
 // and it's numbers only because US
+const checkPostCode = (zip) => {
+  if (Number.isInteger(parseFloat(zip)) && zip.length === 5) {
+    return true;
+  }
+  else {
+    updateUIWithWeather();
+    return false;
+  }
+}
+
+
 
 const checkWeather = async (e) => {
   e.preventDefault();
   const zip = document.getElementById('zip').value;
-  console.log(zip);
+  const isItValid = checkPostCode(zip);
   const url = 'http://localhost:8080/weather/';
-  const result = await fetch(url+zip);
-  try {
-    const data = await result.json();
-    console.log(data);
-  }
-  catch (error) {
-    console.log(error);
+  if (isItValid) {
+    const result = await fetch(url+zip);
+    try {
+      const data = await result.json();
+      console.log(data);
+      updateUIWithWeather(data);
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
 }
 
 const addListeners = () => {
-  document.getElementById('weatherForm').addEventListener('submit', checkWeather)
+  document.getElementById('generate').addEventListener('click', checkWeather);
+  document.getElementById('zip').addEventListener('blur', checkWeather);
 }
 
 // do the things
